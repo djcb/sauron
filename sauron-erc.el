48067c5280b35a39ee33ca4f5c494309ec32bf69
djcb <djcb@djcbsoftware.nl>
2011-12-11 14:47:23 +0200

    * sauron-erc.el: de-prio root messages (bitlbee), experimental join/quit/part   tracking (WIP)

--- sauron-erc.el
+++ sauron-erc.el
@@ -48,14 +48,62 @@ The following events are erc-track
     (message "sauron-erc not available")
     (unless sr-erc-running
       (add-hook 'erc-server-PRIVMSG-functions 'sr-erc-PRIVMSG-hook-func)
+      (add-hook 'erc-server-JOIN-functions 'sr-erc-JOIN-hook-func)
+      (add-hook 'erc-server-PART-functions 'sr-erc-PART-hook-func)
+      (add-hook 'erc-server-QUIT-functions 'sr-erc-QUIT-hook-func)
       (setq sr-erc-running t))))
 
 (defun sauron-erc-stop ()
   "Stop watching ERC."
   (when sr-erc-running
     (remove-hook 'erc-server-PRIVMSG-functions 'sr-erc-PRIVMSG-hook-func)
+    (remove-hook 'erc-server-JOIN-functions 'sr-erc-JOIN-hook-func)
+    (remove-hook 'erc-server-PART-functions 'sr-erc-PART-hook-func)
+    (remove-hook 'erc-server-QUIT-functions 'sr-erc-QUIT-hook-func)    
     (setq sr-erc-running nil)))
-    
+
+
+(defun sr-erc-hook-func (proc parsed event)
+  "Hook function, to be called for erc-matched-hook."
+  (let* ( (me     (erc-current-nick))
+	  (sender (car (erc-parse-user (erc-response.sender parsed))))
+	  (target (car (erc-response.command-args parsed)))
+	  (msg (erc-response.contents parsed)))
+    (sauron-add-event
+      'erc
+      3
+      (concat (propertize sender 'face 'sauron-highlight1-face) " has " 
+	(case event
+	  ('quit "quited")
+	  ('part "parted")
+	  ('join "joined")))
+      ;; FIXME: assumes we open separate window
+      (when (eq event 'joined)
+	(lexical-let ((target target)) 
+	  (lambda()  (sauron-switch-to-buffer target))))
+      `(:event   ,event
+	 :sender ,sender
+	 :me     ,me
+	 :target ,target
+	 :msg    ,msg))))
+
+
+(defun sr-erc-JOIN-hook-func (proc parsed)
+  "JOIN hook function."
+  (message "JOIN")
+  (sr-erc-hook-func proc parse 'join))
+
+(defun sr-erc-QUIT-hook-func (proc parsed)
+  "QUIT hook function."
+  (message "QUIT")
+  (sr-erc-hook-func proc parse 'quit))
+
+(defun sr-erc-PART-hook-func (proc parsed)
+  "PART hook function."
+    (message "PART")
+  (sr-erc-hook-func proc parse 'part))
+
+
 (defun sr-erc-PRIVMSG-hook-func (proc parsed)
   "Hook function, to be called for erc-matched-hook."
   (let* ( (me     (erc-current-nick))
@@ -64,9 +112,10 @@ The following events are erc-track
 	  (msg (erc-response.contents parsed))
 	  (prio
 	    (cond
-	      ((string= me target)   4)    ;; private message for me => prio 4
-	      ((string-match me msg) 3)    ;; I'm mentioned => prio 3 (FIXME)
-	      (t                     2)))) ;; default 
+	      ((string= sender "root") 2)    ;; bitlbee stuff
+	      ((string= me target)     4)    ;; private message for me => prio 4
+	      ((string-match me msg)   3)    ;; I'm mentioned => prio 3 (FIXME)
+	      (t                       2)))) ;; default 
     (sauron-add-event
       'erc
       prio
@@ -74,13 +123,15 @@ The following events are erc-track
 	(propertize sender 'face 'sauron-highlight1-face) "→"
 	(propertize target 'face 'sauron-highlight2-face) " "
 	msg)
-      (lexical-let ((target target)) ;; FIXME: assumes we open separate window
-	(lambda()  (sauron-switch-to-buffer target)))
-      `(:event privmsg
+      ;; FIXME: assumes we open separate window
+      (lexical-let ((target target) (me me)) 
+	(lambda()  (sauron-switch-to-buffer
+		     (if (string= target me) sender target))))
+      `(:event   privmsg
 	 :sender ,sender
-	 :me      ,me
+	 :me     ,me
 	 :target ,target
-	 :msg ,msg))))
+	 :msg    ,msg))))
  
 (provide 'sauron-erc)
 
